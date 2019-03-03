6b397684eddbe811984599b85415b1deec58d819
marek <marek@koplus.ee>
2019-03-03 20:58:28 +0200

    asd

--- src/com/koplus/Main.java
+++ src/com/koplus/Main.java
@@ -1,9 +1,6 @@
 package com.koplus;
 
 import java.io.*;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -17,24 +14,23 @@ public class Main {
         int sizeWord = userWordArr.length;
 
         ArrayList<String> result = new ArrayList<>();
-        String[] inputList = getInputList(args[0]);
 
-        int listSize = inputList.length;
-        for(int i = 0; i < listSize; i++) {
-            String word1 = inputList[i];
-            if (word1.length() != sizeWord) continue;
-            char[] currentWord = word1.toLowerCase().toCharArray();
+        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(args[0]), "WINDOWS-1257"));
+
+        String line;
+        while ((line = br.readLine()) != null) {
+            if (line.length() != sizeWord) {
+                continue;
+            }
+            char[] currentWord = line.toLowerCase().toCharArray();
 
             Arrays.sort(currentWord);
 
             if (Arrays.equals(currentWord, userWordArr)) {
-                result.add(inputList[i]);
+                result.add(line);
             }
         }
-        System.out.println((System.currentTimeMillis() - startTime) + " " + String.join(",", result));
-    }
 
-    private static String[] getInputList(String path) throws IOException {
-        return Files.readAllLines(Paths.get(path), Charset.forName("WINDOWS-1257")).toArray(new String[0]);
+        System.out.println((System.currentTimeMillis() - startTime) + " " + String.join(",", result));
     }
 }
